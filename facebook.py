5a73e6ea904fe1d4af2bdc9636cf7fd86ad2abdf
vi000246 <vi000246@hotmail.com>
2018-04-13 15:12:36 +0800

    add facebook .py

---
+++ facebook.py
@@ -0,0 +1,6 @@
+from fbchat import Client
+from fbchat.models import *
+# p.s. 此用法facebook不會出現通知 因此放棄此方法
+client = Client('帳號', '密碼')
+client.send(Message(text='Hi me!'), thread_id=client.uid, thread_type=ThreadType.USER)
+client.logout()
